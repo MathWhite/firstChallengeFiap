20f228db140d100acd7a9ccd18ce911f141516b8
MathWhite <matheusfgc99@gmail.com>
2025-03-27 21:16:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Aula Inaugural
+
+1. Criar FOmulatio seguindo o mockup do Figma
+2. As informações dever"ao ser enviadas por uma requisição POST para:
+   https://fsdt-contact.onrender.com/contact as informações devem ser enviadas no formato JSON.
+   informações devem ser enviadas no formato JSON
+
+3. 
---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("contactForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const nameInputs = document.querySelectorAll(".nameInput");
+    const names = Array.from(nameInputs)
+        .map(input => input.value.trim())
+        .filter(name => name !== "");
+
+    if (names.length === 0) {
+        alert("Preencha pelo menos um nome.");
+        return;
+    }
+
+    const message = document.getElementById("message").value;
+    const body = { names, message };
+
+    try {
+        //const response = await fetch("https://fsdt-contact.onrender.com/contact", {
+        const response = await fetch("https://eoki5cawskca7ra.m.pipedream.net", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        });
+
+        if (response.ok) {
+            alert("Mensagem enviada com sucesso!");
+        } else {
+            alert("Erro ao enviar mensagem.");
+        }
+    } catch (error) {
+        alert("Falha na requisição.");
+    }
+});
